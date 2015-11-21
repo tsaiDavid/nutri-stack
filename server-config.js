b80fa214da07099f89c69d5aa1f712a551a021a2
David Tsai <David@whataniceguy.com>
2015-11-21 15:11:34 -0800

    Setting up basic express server, initialized npm and bower and installing dependenices to a public/lib for client side

---
+++ server-config.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+// app.use(express.static(path.join(__dirname, '/../public')));
+
+app.get('/', function(req, res) {
+  res.send('hello world');
+});
+
+module.exports = app;
