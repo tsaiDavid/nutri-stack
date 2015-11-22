5c60abbbfc0386a161db53e89e3b21c79feeebf4
David Tsai <David@whataniceguy.com>
2015-11-21 16:11:49 -0800

    Creating and requiring Sequelize/postgres database

---
+++ server/db.js
@@ -0,0 +1,17 @@
+var Sequelize = require('sequelize');
+
+/*
+If you haven't done so yet, please go to terminal and enter "psql",
+create an "admin" user with "admin" as the password, then using that -
+go and "CREATE DATABASE nutristack;"
+*/
+
+// Using Sequelize connection URI
+var db = new Sequelize('nutristack', 'admin', 'admin', {
+  dialect: 'postgres',
+
+  // Set below property if you want to enable logging
+  logging: false,
+});
+
+module.exports = db;
--- server/server-config.js
+++ server/server-config.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var db = require('./db');
 var path = require('path');
 var bodyParser = require('body-parser');
 
