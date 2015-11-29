9543f562c1d8e5035343e28ef2873430886134ad
David Tsai <David@whataniceguy.com>
2015-11-29 15:26:16 -0800

    Adding documentation for server-config

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+'use strict';
+var gulp = require('gulp');
+var shell = require('gulp-shell');
+var jshint = require('gulp-jshint');
+
+// var jasmine = require('gulp-jasmine');
+// var stylish = require('jshint-stylish');
+// var jscs = require('gulp-jscs');
+// var isWin = /^win/.test(process.platform);
+
+gulp.task('jsdoc', shell.task([
+  './node_modules/.bin/jsdoc -c ./conf.json',
+]));
+
+// gulp.task('lint', function () {
+//   return gulp.src(['./src/**/*.js'], ['./test/**/*.js'])
+//     .pipe(jshint())
+//     .pipe(jshint.reporter(stylish))
+//     .pipe(jshint.reporter('fail'));
+// });
+//
+// gulp.task('test', function () {
+//   return gulp.src('test/**/*.spec.js')
+//     .pipe(jasmine());
+// });
+//
+// gulp.task('jscs', function () {
+//   return gulp.src(['src/**/*.js', 'test/**/*.js'])
+//     .pipe(jscs());
+// });
+//
+// gulp.task('build', ['lint', 'jscs', 'test']);
