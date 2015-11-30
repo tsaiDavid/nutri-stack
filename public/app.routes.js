982cc383695e9ab3452df6b096f54de96be64faa
David Tsai <David@whataniceguy.com>
2015-11-29 17:51:41 -0800

    Adding additional documentation

--- public/app.routes.js
+++ public/app.routes.js
@@ -1,17 +1,23 @@
+// Follows pattern of wrapping Angular components in IIFE.
 (function() {
   'use strict';
 
+  // Our Angular module was named 'app', here we attach our config
   angular.module('app')
   .config(config);
 
-  // upon document load, enable mobile sidenav; setting to close on click
+  // When document is loaded, enable mobile sidenav
+  //  * closeOnClick set to true, when user clicks off, nav will shrink
   $('.button-collapse').sideNav({closeOnClick: true});
 
   config.$inject = ['$stateProvider', '$urlRouterProvider'];
 
+  // Configure the UI Routing
   function config($stateProvider, $urlRouterProvider) {
     $urlRouterProvider.otherwise('/home');
 
+    // The controllers and controller as syntax is specified in the router,
+    // so controllers will only be instantiated once per page load
     $stateProvider
       .state('home', {
         url: '/home',
@@ -31,12 +37,17 @@
         controller: 'StackCtrl',
         controllerAs: 'vm',
       })
+
+      // **username** is used here as a route param
       .state('stacks.list', {
         url: '/:username',
         templateUrl: 'stacks/stacks.stackslist.html',
         controller: 'StackCtrl',
         controllerAs: 'vm',
       })
+
+      // to get the unique stack, we base it off the **username** route and add
+      // the unique **title** that is desired for retrieval
       .state('uniqueStack', {
         url: '/stacks/:username/:title',
         templateUrl: 'uniqueStack/uniqueStack.html',
